6186e9ae576621804634c21c1d8e5b327ed9af02
Dung Ta Van <itprono3@gmail.com>
2020-07-14 22:36:09 +0700

    add AESCBC Example

---
+++ src/main/java/com/tvd12/example/aes/AESCBCExample.java
@@ -0,0 +1,81 @@
+package com.tvd12.example.aes;
+
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AESCBCExample {
+	
+	public static void main(String[] args) throws Exception {
+        String key = "helloworld";
+        String clean = "Think better, Do better";
+        
+        byte[] encrypted = encrypt(clean, key);
+        System.out.println("encrypted: " + Base64.getEncoder().encodeToString(encrypted));
+        String decrypted = decrypt(encrypted, key);
+        System.out.println("decrypted: " + decrypted);
+    }
+
+    public static byte[] encrypt(String plainText, String key) throws Exception {
+        byte[] clean = plainText.getBytes();
+
+        // Generating IV.
+        int ivSize = 16;
+        byte[] iv = new byte[ivSize];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(iv);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+        // Hashing key.
+        byte[] keyBytes = hashKey(key);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
+
+        // Encrypt.
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+        byte[] encrypted = cipher.doFinal(clean);
+
+        // Combine IV and encrypted part.
+        byte[] encryptedIVAndText = new byte[ivSize + encrypted.length];
+        System.arraycopy(iv, 0, encryptedIVAndText, 0, ivSize);
+        System.arraycopy(encrypted, 0, encryptedIVAndText, ivSize, encrypted.length);
+
+        return encryptedIVAndText;
+    }
+
+    public static String decrypt(byte[] encryptedIvTextBytes, String key) throws Exception {
+        int ivSize = 16;
+
+        // Extract IV.
+        byte[] iv = new byte[ivSize];
+        System.arraycopy(encryptedIvTextBytes, 0, iv, 0, iv.length);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+        // Extract encrypted part.
+        int encryptedSize = encryptedIvTextBytes.length - ivSize;
+        byte[] encryptedBytes = new byte[encryptedSize];
+        System.arraycopy(encryptedIvTextBytes, ivSize, encryptedBytes, 0, encryptedSize);
+
+        // Hash key.
+        byte[] keyBytes = hashKey(key);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
+
+        // Decrypt.
+        Cipher cipherDecrypt = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipherDecrypt.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+        byte[] decrypted = cipherDecrypt.doFinal(encryptedBytes);
+
+        return new String(decrypted);
+    }
+    
+    private static byte[] hashKey(String key) throws Exception {
+    	MessageDigest digest = MessageDigest.getInstance("MD5");
+        digest.update(key.getBytes("UTF-8"));
+        return digest.digest();
+    }
+
+}
