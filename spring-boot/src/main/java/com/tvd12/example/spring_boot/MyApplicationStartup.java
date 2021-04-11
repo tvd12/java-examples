72fc61195f10c6df5c8da052e14314e0c97ea710
Dung Ta Van <itprono3@gmail.com>
2021-04-11 16:36:59 +0700

    add spring-boot

--- spring-boot/src/main/java/com/tvd12/example/spring_boot/MyApplicationStartup.java
+++ spring-boot/src/main/java/com/tvd12/example/spring_boot/MyApplicationStartup.java
@@ -4,9 +4,18 @@ import com.tvd12.example.spring_boot.entity.Book;
 import com.tvd12.example.spring_boot.service.BookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableAutoConfiguration(
+    exclude = {
+        RestTemplateAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    }
+)
 @ComponentScan
 @SpringBootConfiguration
 public class MyApplicationStartup {
